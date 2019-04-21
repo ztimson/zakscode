a0951f82424b1b5f3af0313f7f3832be05fd465e
ztimson <zaktimson@gmail.com>
2019-04-21 16:59:20 -0400

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -7,6 +9,7 @@ import {MaterialModule} from './material.module';
 import {FormsModule} from '@angular/forms';
 import {TypewriterComponent} from './components/typewriter/typewriter.component';
 import {SlideShowComponent} from './components/slideShow/slideShow.component';
+import {environment} from '../environments/environment';
 
 @NgModule({
     declarations: [
@@ -15,6 +18,8 @@ import {SlideShowComponent} from './components/slideShow/slideShow.component';
         TypewriterComponent
     ],
     imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
@@ -23,5 +28,4 @@ import {SlideShowComponent} from './components/slideShow/slideShow.component';
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyC1vKmDDzRR85DnOowPZvAkZEyzJ1EmjGI',
+        authDomain: 'zaks-code.firebaseapp.com',
+        databaseURL: 'https://zaks-code.firebaseio.com',
+        projectId: 'zaks-code',
+        storageBucket: 'zaks-code.appspot.com',
+        messagingSenderId: '430075133237'
+    }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
