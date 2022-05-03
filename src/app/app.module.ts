7c398632f7b9681c9bd0d714654a06c49cda3333
ztimson <zaktimson@gmail.com>
2022-05-03 18:07:45 -0400

    Added contact form (Closes #3)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,35 @@ import {ConsoleComponent} from './components/console/console.component';
     bootstrap: [AppComponent]
 })
 export class AppModule { }
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ContactFormComponent} from './components/contact-form/contact-form.component';
+import {HomeComponent} from './views/home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import {FormsModule} from '@angular/forms';
+import {TypewriterComponent} from './components/typewriter/typewriter.component';
+import {SlideShowComponent} from './components/slideShow/slideShow.component';
+import {AppComponent} from './views/app/app.component';
+import {AppRouting} from './app.routing';
+import {ConsoleComponent} from './components/console/console.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        ConsoleComponent,
+		ContactFormComponent,
+        HomeComponent,
+        SlideShowComponent,
+        TypewriterComponent
+    ],
+    imports: [
+        AppRouting,
+        BrowserModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MaterialModule,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
