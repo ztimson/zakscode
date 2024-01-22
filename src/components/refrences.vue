e055e306df2b9229e358bf6f03a303cd05899d79
ztimson <zaktimson@gmail.com>
2024-01-22 14:23:54 -0500

    Added Andre's references

--- src/components/refrences.vue
+++ src/components/refrences.vue
@@ -3,7 +3,7 @@
 const resume = 'https://drive.google.com/file/d/1km4XqtXrYtoUnrDpEtDmAeKWh1qyp9nQ/view?usp=drive_link';
 
 const refrences = [
-	// ['Andre Mourinho', ''],
+	['Andre Mourinho', 'https://drive.google.com/file/d/1QSRzBg6xj1evwIhpAkrKZqPrbJOjwkYR/view?usp=drive_link'],
 	['CD Projekt Red', 'https://drive.google.com/file/d/1QT02DBRFg4HcJXoxuH0WfiozjYHoJWrn/view?usp=drive_link'],
 	['Chris Cartwright', 'https://drive.google.com/file/d/17JtSlVvMt_84XG0iQO8cHlDsi30oR34c/view?usp=drive_link'],
 	['Garry Whyte', 'https://drive.google.com/file/d/18Mqv1vX2wmseQkqEPRIZiuSuGhDri-dB/view?usp=drive_link'],
@@ -21,7 +21,7 @@ const refrences = [
 			<a v-for="ref in refrences" class="btn btn-outline-primary" :href="ref[1]" target="_blank">{{ref[0]}}</a>
 		</div>
 	</div>
-	<div class="d-none d-md-block">
+	<div class="d-none d-md-flex align-items-start">
 		<a class="btn btn-outline-danger me-3" :href="resume" target="_blank">CSV / Resume</a>
 		<div class="btn-group" role="group">
 			<a v-for="ref in refrences" class="btn btn-outline-primary" :href="ref[1]" target="_blank">{{ref[0]}}</a>
