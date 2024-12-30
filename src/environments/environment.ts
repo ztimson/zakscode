8d84fc8274fdf99333f0958bd543c80679de657c
ztimson <zaktimson@gmail.com>
2024-12-30 12:19:57 -0500

    Fix momentum config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 const devMode = location?.port == '5173';
 
 export const environment = {
-	apiUrl: devMode ? 'http://localhost' : location.host,
+	apiUrl: devMode ? 'http://localhost' : location.origin,
 	devMode
 }
